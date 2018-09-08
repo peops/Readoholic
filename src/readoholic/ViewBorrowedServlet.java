b1ef07c92ed5499bf918dde6b47a3c204a3edf82
Ankit Lohani <ankit.lohani.iitkgp@gmail.com>
2018-09-08 18:47:03 +0530

    View Boorowed servlet integrated with sessions

--- src/readoholic/ViewBorrowedServlet.java
+++ src/readoholic/ViewBorrowedServlet.java
@@ -19,24 +19,44 @@ public class ViewBorrowedServlet extends HttpServlet {
     	conn = DBConnection.getStatement();
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession hs=request.getSession(false);
-		User user=(User)hs.getAttribute("login_user");
-		
-		List<Asset> assets = DBMethod.read_borrowed_assets(conn, user.getUserName());
-
-		RequestDispatcher rd = request.getRequestDispatcher("ViewBorrowed.jsp");
-		request.setAttribute("assetlist",assets);
-		rd.forward(request, response);
+		HttpSession hs=request.getSession(false);
+		if(hs!=null){ 
+			User user=(User)hs.getAttribute("login_user");
+			String sessionID = null;
+			String username = null;
+			Cookie[] cookies = request.getCookies();
+			if(cookies !=null){
+				for(Cookie cookie : cookies){
+					if(cookie.getName().equals("user")) username = cookie.getValue();
+					if(cookie.getName().equals("JSESSIONID")) sessionID = cookie.getValue();
+				}
+			}
+			List<Asset> assets = DBMethod.read_borrowed_assets(conn, username);
+			RequestDispatcher rd = request.getRequestDispatcher("ViewBorrowed.jsp");
+			request.setAttribute("assetlist",assets);
+			rd.forward(request, response);
+        }
+        else{
+        	RequestDispatcher rd =request.getRequestDispatcher("Login.jsp");
+			request.setAttribute("msg","Log-In first!");
+			rd.forward(request, response);
+        }
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    HttpSession hs=request.getSession(false);
-		@SuppressWarnings("unused")
+		HttpSession hs=request.getSession(false);
 		User user=(User)hs.getAttribute("login_user");
-		
+		String sessionID = null;
+		String username = null;
+		Cookie[] cookies = request.getCookies();
+		if(cookies !=null){
+			for(Cookie cookie : cookies){
+				if(cookie.getName().equals("user")) username = cookie.getValue();
+				if(cookie.getName().equals("JSESSIONID")) sessionID = cookie.getValue();
+			}
+		}
 		String id=request.getParameter("return");
 		DBMethod.edit_asset_status(conn, id, false, false);
 		DBMethod.edit_asset_borrower(conn, id, null);
-		
 		RequestDispatcher rd = request.getRequestDispatcher("MainMenu.jsp");
 		rd.forward(request, response);
 	}
