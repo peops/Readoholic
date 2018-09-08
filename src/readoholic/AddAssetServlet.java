ad89d4d7d61dda44700a9b85236245289acc42d5
Ankit Lohani <ankit.lohani.iitkgp@gmail.com>
2018-09-08 18:17:39 +0530

    integrated addassetservlet with sessions and cookies

--- src/readoholic/AddAssetServlet.java
+++ src/readoholic/AddAssetServlet.java
@@ -20,21 +20,34 @@ public class AddAssetServlet extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String assetname=request.getParameter("assetname");
-		String assetclass=request.getParameter("class");
-		String description=request.getParameter("description");
-		String sec=request.getParameter("security");
-		int security=Integer.parseInt(sec);
-		
 		HttpSession hs=request.getSession(false);
-		User user=(User)hs.getAttribute("login_user");
-		
-		String ownerId=user.getUserName();
-		Asset asset=new Asset(assetname,assetclass,ownerId,description,security);
-		
-		DBMethod.add_asset(conn, asset);
-		
-		RequestDispatcher rd=request.getRequestDispatcher("MainMenu.jsp");
-		rd.forward(request, response);
+		if(hs!=null){ 
+			User user=(User)hs.getAttribute("login_user");
+			String sessionID = null;
+			String username = null;
+			Cookie[] cookies = request.getCookies();
+			if(cookies !=null){
+				for(Cookie cookie : cookies){
+					if(cookie.getName().equals("user")) username = cookie.getValue();
+					if(cookie.getName().equals("JSESSIONID")) sessionID = cookie.getValue();
+				}
+			}
+			
+			String assetname=request.getParameter("assetname");
+			String assetclass=request.getParameter("class");
+			String description=request.getParameter("description");
+			String sec=request.getParameter("security");
+			int security=Integer.parseInt(sec);
+			
+			Asset asset=new Asset(assetname,assetclass,username,description,security);
+			DBMethod.add_asset(conn, asset);
+			RequestDispatcher rd=request.getRequestDispatcher("MainMenu.jsp");
+			rd.forward(request, response);
+        }
+        else{
+        	RequestDispatcher rd =request.getRequestDispatcher("Login.jsp");
+			request.setAttribute("msg","Log-In first!");
+			rd.forward(request, response);
+        }
 	}
 }
